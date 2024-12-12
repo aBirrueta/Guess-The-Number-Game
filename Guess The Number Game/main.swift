89040094f8c8a13614e26d0da201b8720d503afc
aBirrueta <alejandrobirru3ta@gmail.com>
2024-12-12 10:09:50 -0800

    working on loop and if statements

--- Guess The Number Game/main.swift
+++ Guess The Number Game/main.swift
@@ -11,7 +11,21 @@ print("Welcome to The Guess the Number Game!")
 print("Guess a number between 1 and 100.")
 
 var randomNum = Int.random(in: 1...100)
+print(randomNum)
 
-var answer = readLine()!
+var gameNotOver = true
+var guess: Int?
 
+while gameNotOver{
+    var number = readLine()!
+    guess = Int(number)
+    
+    if guess == randomNum {
+        print("you guessed the number right GOOD JOB!")
+        gameNotOver = false
+    }
+    if guess < randomNum{
+        print("Your guess was too little guess again.")
+    }
+}
 
