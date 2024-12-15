f9d5f884f6691755169d00546d581815af3a8249
aBirrueta <alejandrobirru3ta@gmail.com>
2024-12-15 11:48:20 -0800

    fixed errors

--- Guess The Number Game/main.swift
+++ Guess The Number Game/main.swift
@@ -9,34 +9,35 @@ import Foundation
 
 print("Welcome to The Guess the Number Game!")
 print("Guess a number between 1 and 100.")
-
+print("Enter your guess below")
 var randomNum = Int.random(in: 1...100)
 
 var gameNotOver = true
 var guess: Int
 var guessCount = 0
 
-while gameNotOver{
+while gameNotOver {
     var number = readLine()!
     var maybeGuess = Int(number)
+    
     if maybeGuess == nil {
-        print("Thats not a number try again.")
+        print("Thats not a number try again")
     }
     else {
-        guessCount = guessCount+1
+        guessCount = guessCount + 1
         guess = maybeGuess!
+        
         if guess == randomNum {
             print("you guessed the number right GOOD JOB!")
-            print("You guessed "+String(guessCount)+" times to get it right.")
+            print("You guessed " + String(guessCount) + " times to get it right.")
             gameNotOver = false
-            
         }
-        if guess < randomNum{
-            print("Your guess is too little guess again.")
-        }
-        if guess > randomNum{
-            print("Your guess is too big guess again.")
         
+        if guess < randomNum {
+            print("Your guess is too little guess again")
+        }
+        if guess > randomNum {
+            print("Your guess is too big guess again")
         }
     }
 }
